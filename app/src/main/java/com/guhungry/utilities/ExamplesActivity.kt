96ed03a90f72e8204fe8487776afeb1ff1f26921
Woraphot Chokratanasombat <wc@asiola.com>
2018-08-04 01:32:56 +0700

    Initial import

---
+++ app/src/main/java/com/guhungry/utilities/ExamplesActivity.kt
@@ -0,0 +1,11 @@
+package com.guhungry.utilities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExamplesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_examples)
+    }
+}
