0ff97fbdbbb9a97f301a55eb05ca6a6d762620f3
Woraphot Chokratanasombat <wc@asiola.com>
2018-08-11 01:19:08 +0700

    [RxJava Example] Change: Add example for suspendableInterval, resubscribe on resume, clear and dispose subscriber

--- app/src/main/java/com/guhungry/utilities/app/rxjava/RxJavaActivity.kt
+++ app/src/main/java/com/guhungry/utilities/app/rxjava/RxJavaActivity.kt
@@ -1,12 +1,69 @@
 package com.guhungry.utilities.app.rxjava
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.guhungry.utilities.R
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.functions.BiFunction
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_rx_java.*
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicBoolean
 
 class RxJavaActivity : AppCompatActivity() {
+    private val disposables = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx_java)
+
+        blank_activity.setOnClickListener { showBlankActivity() }
+    }
+
+    private fun showBlankActivity() {
+        startActivity(Intent(this, RxJavaBlankActivity::class.java))
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        active.set(true)
+        setupExample1()
+    }
+
+    private val active = AtomicBoolean(true)
+    private val ticker = Flowable.interval(1, TimeUnit.SECONDS)
+            .takeWhile { active.get() }
+            .repeat()
+    private val observable1 = Flowable.zip(
+            Flowable.range(1, 100),
+            ticker,
+            BiFunction<Int, Long, Int> { int, _ -> int }
+    )
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .publish()
+            .autoConnect()
+
+    private fun setupExample1() {
+        disposables.add(
+                observable1.subscribe { rxjava1.text = "%s %d".format(rxjava1.text, it) }
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        active.set(false)
+        disposables.clear()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if (!disposables.isDisposed) disposables.dispose()
     }
 }
---
+++ app/src/main/java/com/guhungry/utilities/app/rxjava/RxJavaBlankActivity.kt
@@ -0,0 +1,13 @@
+package com.guhungry.utilities.app.rxjava
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.guhungry.utilities.R
+
+class RxJavaBlankActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rx_java_blank)
+    }
+}
