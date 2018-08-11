dbc5cfa97f46c1b23c9bed7a1dc2e42f386bed29
Woraphot Chokratanasombat <wc@asiola.com>
2018-08-12 00:15:56 +0700

    [RxJava Example] Change: Add more examples

--- app/src/main/java/com/guhungry/utilities/app/rxjava/RxJavaActivity.kt
+++ app/src/main/java/com/guhungry/utilities/app/rxjava/RxJavaActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.guhungry.utilities.R
+import com.jakewharton.rxbinding2.view.clicks
+import com.jakewharton.rxbinding2.widget.textChanges
 import io.reactivex.Flowable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -19,8 +21,6 @@ class RxJavaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx_java)
-
-        blank_activity.setOnClickListener { showBlankActivity() }
     }
 
     private fun showBlankActivity() {
@@ -30,11 +30,14 @@ class RxJavaActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-        active.set(true)
         setupExample1()
+        setupExample2()
+        setupExample3()
+        setupExample4()
+        active.set(true)
     }
 
-    private val active = AtomicBoolean(true)
+    private val active = AtomicBoolean(false)
     private val ticker = Flowable.interval(1, TimeUnit.SECONDS)
             .takeWhile { active.get() }
             .repeat()
@@ -47,10 +50,34 @@ class RxJavaActivity : AppCompatActivity() {
             .observeOn(AndroidSchedulers.mainThread())
             .publish()
             .autoConnect()
-
     private fun setupExample1() {
         disposables.add(
                 observable1.subscribe { rxjava1.text = "%s %d".format(rxjava1.text, it) }
+        )
+    }
+
+    private val observable2 = observable1.filter { it % 2 == 0 }
+    private fun setupExample2() {
+        disposables.add(
+                observable2.subscribe { rxjava2.text = "%s %d".format(rxjava2.text, it) }
+        )
+    }
+
+    private val observable3 by lazy { rxjava3_edit.textChanges()
+            .skipInitialValue()
+            .debounce (500, TimeUnit.MILLISECONDS)
+            .filter { it.length > 3 } }
+    private fun setupExample3() {
+        disposables.add(
+                observable3.subscribe { rxjava3.text = it }
+        )
+    }
+
+    private val observable4 by lazy { blank_activity.clicks()
+            .debounce(2, TimeUnit.SECONDS) }
+    private fun setupExample4() {
+        disposables.add(
+                observable4.subscribe { showBlankActivity() }
         )
     }
 
