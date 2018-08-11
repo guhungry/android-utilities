9da969e4bfc43483773f103f0bc6ca2304dbe807
Woraphot Chokratanasombat <wc@asiola.com>
2018-08-12 00:21:19 +0700

    [Retrofit Example] Change: Add blank layout

---
+++ app/src/main/java/com/guhungry/utilities/app/retrofit/RetrofitActivity.kt
@@ -0,0 +1,13 @@
+package com.guhungry.utilities.app.retrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.guhungry.utilities.R
+
+class RetrofitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_retrofit)
+    }
+}
